6d07869984dd8ef87622ef245bc6c6dcfb7653eb
Ajit Tawde <43417358+atawde@users.noreply.github.com>
2026-07-18 20:17:26 +0530

    Add seed_admin_router to main application

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from .routes.admin import router as admin_router
 from .routes.student import router as student_router
 from .routes.auth import router as auth_router
 from .routes.home import router as home_router
+from .routes.setup_seed import router as seed_admin_router  
 from .db.base import Base
 from .db.session import engine
 
@@ -17,6 +18,7 @@ app.include_router(admin_router)
 app.include_router(student_router)
 app.include_router(auth_router)
 app.include_router(home_router)
+app.include_router(seed_admin_router)
 # app.include_router(jobs_router)
 
 
