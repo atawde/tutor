ccc7733038dbda3b161577b7f780b35d871052e3
Ajit Tawde <43417358+atawde@users.noreply.github.com>
2026-07-18 20:58:33 +0530

    Update setup_seed.py

--- app/routes/setup_seed.py
+++ app/routes/setup_seed.py
@@ -11,38 +11,34 @@ def setup_admin():
 
     db: Session = SessionLocal()
 
-    try:
+        try:
         existing_user = db.query(User).filter(
             User.name == "Administrator"
         ).first()
 
-        if existing_user:
-            return {"message": "Admin already exists"}
-
-        admin = User(
-            name="Administrator",
-            email="admin@tutor24x7.com",
-            password_hash=hash_password("admin123"),
-            role="admin"
-        )
-
+        if not existing_user:
+            admin = User(
+               name="Administrator",
+               email="admin@tutor24x7.com",
+               password_hash=hash_password("admin123"),
+               role="admin"
+            )
+            db.add(admin)
+            db.commit()
+            
         existing_user = db.query(User).filter(
             User.name == "Student"
         ).first()
 
-        if existing_user:
-            return {"message": "Student already exists"}
-
-        student = User(
-            name="Student",
-            email="student@tutor24x7.com",
-            password_hash=hash_password("student123"),
-            role="student"
-        )
-
-        db.add(admin)
-        db.add(student)
-        db.commit()
+        if not existing_user:
+            student = User(
+                name="Student",
+                email="student@tutor24x7.com",
+                password_hash=hash_password("student123"),
+                role="student"
+            )
+            db.add(student)
+            db.commit()
 
         return {"message": "Admin and student created successfully"}
 
