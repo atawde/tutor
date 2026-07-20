4af6d1ab2d53498930fea5c995caec5addfbc0ac
Ajit Tawde <43417358+atawde@users.noreply.github.com>
2026-07-20 20:00:42 +0530

    Update setup_seed.py

--- app/routes/setup_seed.py
+++ app/routes/setup_seed.py
@@ -12,36 +12,33 @@ def setup_admin():
     db: Session = SessionLocal()
 
     try:
-            existing_user = db.query(User).filter(
-                User.name == "Administrator"
-            ).first()
+        existing_user = db.query(User).filter(
+            User.name == "Administrator"
+        ).first()
 
-            if not existing_user:
-                admin = User(
-                   name="Administrator",
-                   email="admin@tutor24x7.com",
-                   password_hash=hash_password("admin123"),
-                   role="admin"
-                )
+        if not existing_user:
+             admin = User(
+                name="Administrator",
+                email="admin@tutor24x7.com",
+                password_hash=hash_password("admin123"),
+                role="admin"
+             )
+             db.add(admin)
+             db.commit()
             
-                db.add(admin)
-                db.commit()
-            
-            existing_user = db.query(User).filter(
-                User.name == "Student"
-            ).first()
+        existing_user = db.query(User).filter(
+            User.name == "Student"
+        ).first()
 
-            if not existing_user:
-                student = User(
-                    name="Student",
-                    email="student@tutor24x7.com",
-                    password_hash=hash_password("student123"),
-                    role="student"
-                )
-                db.add(student)
-                db.commit()
-                
-            return {"message": "Admin and student created successfully"}
-
+        if not existing_user:
+             student = User(
+                 name="Student",
+                 email="student@tutor24x7.com",
+                 password_hash=hash_password("student123"),
+                 role="student"
+             )
+             db.add(student)
+             db.commit()              
     finally:
         db.close()
+        return {"message": "Admin and student created successfully"}
