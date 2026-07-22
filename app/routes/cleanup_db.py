15b1b964e893d6304da71fcb314f21e29ab37dec
Ajit Tawde <43417358+atawde@users.noreply.github.com>
2026-07-22 13:51:48 +0530

    Update cleanup_db.py

--- app/routes/cleanup_db.py
+++ app/routes/cleanup_db.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.db.session import SessionLocal
 from app.models.subject import Subject
 from app.models.job import ProcessingJob
+from app.models.user import User
 from app.models.audio_lesson import AudioLesson
 
 router = APIRouter()
@@ -17,6 +18,7 @@ def cleanup_db():
         db.query(Subject).delete()
         db.query(AudioLesson).delete()
         db.query(ProcessingJob).delete()
+        db.query(User).delete()
 
         db.commit()
     finally:
